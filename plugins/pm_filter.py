04658534ec4603b0fabb7e7ba8445ffbc99bd42c
Dhoniboy <103166082+Dhoniboy@users.noreply.github.com>
2023-01-21 23:23:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -112,7 +112,7 @@ async def next_page(bot, query):
                         text=f"{file.file_name}", url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
                     ),
                     InlineKeyboardButton(
-                        text=f"{get_size(file.file_size)}",
+                        text=f"📂{get_size(file.file_size)}📥",
                         url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
                     ),
                 ]
@@ -135,7 +135,7 @@ async def next_page(bot, query):
                         text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
                     ),
                     InlineKeyboardButton(
-                        text=f"{get_size(file.file_size)}",
+                        text=f"📂{get_size(file.file_size)}📥",
                         callback_data=f'files_#{file.file_id}',
                     ),
                 ]
